f68cf2db8b3c691be23422000e246e2f70d26bf8
Kirill Tankovskiy <tankovskiyko@yandex-team.ru>
2023-11-13 15:45:34 +0300

    Initial commit

---
+++ src/main/java/com/itmo/tinkoffinvestementbot/TinkoffInvestementBotApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.tinkoffinvestementbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffInvestementBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffInvestementBotApplication.class, args);
+    }
+
+}
