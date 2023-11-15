b75c44d42999a36f510707eba7a2c74a58a8dcc8
Mikhail Shadrin <mdshadrin@gmail.com>
2023-11-15 20:23:06 +0300

    Rolled back deletion of strategy-service-boot

---
+++ strategy-service-boot/src/main/java/com/itmo/StrategyApplication.java
@@ -0,0 +1,11 @@
+package com.itmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyApplication.class, args);
+    }
+}
