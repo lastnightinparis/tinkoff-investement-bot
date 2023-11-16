72e62e1109071f4f3e1122c459811c2c536a89fd
Mikhail Shadrin <mdshadrin@gmail.com>
2023-11-16 19:04:00 +0300

    Added run configs, updated spring boot version

--- orchestrator-service-boot/src/main/java/com/itmo/tinkoffinvestementbot/controller/TokenController.java
+++ orchestrator-service-boot/src/main/java/com/itmo/tinkoffinvestementbot/controller/TokenController.java
@@ -2,12 +2,14 @@ package com.itmo.tinkoffinvestementbot.controller;
 
 import com.itmo.tinkoffinvestementbot.service.TokenService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tinkoffinvestementbot.dto.bot.ValidateTokenResponse;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class TokenController {
@@ -16,8 +18,9 @@ public class TokenController {
 
     @GetMapping("/token/check")
     public ValidateTokenResponse checkToken(@RequestParam(name = "userId") Long userId, @RequestParam(name = "token") String token) {
-
-        return new ValidateTokenResponse(tokenService.checkToken(userId, token));
+        var checkResult = tokenService.checkToken(userId, token);
+        log.info("Проверили токен {}. Результат: {}", token, checkResult);
+        return new ValidateTokenResponse(checkResult);
     }
 
     @DeleteMapping("/token/delete")
